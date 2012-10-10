6122a91b66fa15fe689a222930c661bf0d055500
Scott Dupoy <git@dupoy.com>
2012-10-10 21:00:05 +0100

    make strava wrapper a bit more oo

--- uploader/strava-wrapper.rb
+++ uploader/strava-wrapper.rb
@@ -2,41 +2,60 @@
 
 require 'selenium-webdriver'
 
-driver = Selenium::WebDriver.for :chrome
+#driver.save_screenshot('manual-upload.png')
 
-# login
-puts 'navigating to login page'
-driver.navigate.to 'https://www.strava.com/login'
-puts 'navigated to login page'
+class StravaWrapper
 
-# email address
-puts 'finding email element'
-element = driver.find_element(:id, 'email')
-puts 'setting email'
-element.clear
-element.send_keys 'email-address-goes-here'
+    def connect
+        @driver = Selenium::WebDriver.for :chrome
+        
+        puts 'navigating to login page'
+        @driver.navigate.to 'https://www.strava.com/login'
+        puts 'navigated to login page'
+    end
 
-# password
-puts 'finding password element'
-element = driver.find_element(:id, 'password')
-puts 'setting password'
-element.clear
-element.send_keys 'password-goes-here'
+    def login(email, password)
+        # email address
+        puts 'finding email element'
+        element = @driver.find_element(:id, 'email')
+        puts 'setting email'
+        element.clear
+        element.send_keys email
 
-# click submit button
-element = driver.find_element(:xpath, '//button')
-element.click
+        # password
+        puts 'finding password element'
+        element = @driver.find_element(:id, 'password')
+        puts 'setting password'
+        element.clear
+        element.send_keys password
 
-# navigate to manual activity upload page
-puts 'navigating to manual upload page'
-driver.navigate.to 'http://app.strava.com/activities/new'
-puts 'navigated to manual upload page'
+        # click submit button
+        element = @driver.find_element(:xpath, '//button')
+        element.click
+    end
 
-puts 'saving screenshot'
-driver.save_screenshot('manual-upload.png')
+    def close
+        puts 'closing'
+        driver.close
+        puts 'closed'
+    end
 
-puts 'closing'
-driver.close
-puts 'closed'
+    def upload_run(run)
+        puts 'uploading run: '
+        
+        # navigate to manual activity upload page
+        puts '  navigating to manual upload page'
+        driver.navigate.to 'http://app.strava.com/activities/new'
+        puts '  navigated to manual upload page'
+    end
 
+    def upload_ride(ride)
+        puts 'uploading ride: '
 
+        # navigate to manual activity upload page
+        puts '  navigating to manual upload page'
+        driver.navigate.to 'http://app.strava.com/activities/new'
+        puts '  navigated to manual upload page'
+    end
+
+end
