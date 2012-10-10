9f0507356dc57252329ffd73a3e8785b7b8883fd
Scott Dupoy <git@dupoy.com>
2012-10-10 20:41:45 +0100

    order database retrievals

--- database-pull/database.rb
+++ database-pull/database.rb
@@ -26,7 +26,7 @@ class Database
 
     def retrieve_runs
         runs = Array.new
-        results = @client.query("select ts.date, tr.distance, tr.time from t_sessions ts inner join t_running tr on ts.subsession_id = tr.sub_session_id where ts.user_id = 1 and ts.type_id = 1;")
+        results = @client.query("select ts.date, tr.distance, tr.time from t_sessions ts inner join t_running tr on ts.subsession_id = tr.sub_session_id where ts.user_id = 1 and ts.type_id = 1 order by ts.date desc;")
         results.each do |row|
             runs << Run.new(row['date'], row['distance'], row['time'])
         end
@@ -35,7 +35,7 @@ class Database
 
     def retrieve_rides
         rides = Array.new
-        results = @client.query("select ts.date, tc.distance, tc.max_speed, tc.avg_speed from t_sessions ts inner join t_cycling tc on ts.subsession_id = tc.sub_session_id where ts.user_id = 1 and ts.type_id = 3;")
+        results = @client.query("select ts.date, tc.distance, tc.max_speed, tc.avg_speed from t_sessions ts inner join t_cycling tc on ts.subsession_id = tc.sub_session_id where ts.user_id = 1 and ts.type_id = 3 order by ts.date desc;")
         results.each do |row|
             rides << Ride.new(row['date'], row['distance'], row['max_speed'], row['avg_speed'])
         end
