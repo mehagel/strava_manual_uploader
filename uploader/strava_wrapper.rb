34771bee9c5c3d8a1eb12272b0d2be91b231981a
Scott Dupoy <git@dupoy.com>
2012-10-14 07:22:58 +0100

    Working and used to upload all data

--- uploader/strava_wrapper.rb
+++ uploader/strava_wrapper.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 require 'selenium-webdriver'
 
 #driver.save_screenshot('manual-upload.png')
@@ -9,53 +7,121 @@ class StravaWrapper
     def connect
         @driver = Selenium::WebDriver.for :chrome
         
-        puts 'navigating to login page'
+        puts 'Navigating to login page'
         @driver.navigate.to 'https://www.strava.com/login'
-        puts 'navigated to login page'
+        puts 'Navigated to login page'
     end
 
     def login(email, password)
         # email address
-        puts 'finding email element'
+        puts 'Finding email element'
         element = @driver.find_element(:id, 'email')
-        puts 'setting email'
+        puts 'Setting email'
         element.clear
         element.send_keys email
 
         # password
-        puts 'finding password element'
+        puts 'Finding password element'
         element = @driver.find_element(:id, 'password')
-        puts 'setting password'
+        puts 'Setting password'
         element.clear
         element.send_keys password
 
         # click submit button
         element = @driver.find_element(:xpath, '//button')
+        puts 'Logging in'
         element.click
+        puts 'Logged in'
     end
 
     def close
-        puts 'closing'
-        driver.close
-        puts 'closed'
+        puts 'Closing'
+        @driver.close
+        puts 'Closed'
     end
 
     def upload_run(run)
-        puts 'uploading run: '
-        
+        puts 'Uploading run:'
+
         # navigate to manual activity upload page
-        puts '  navigating to manual upload page'
-        driver.navigate.to 'http://app.strava.com/activities/new'
-        puts '  navigated to manual upload page'
+        puts '  Navigating to manual upload page'
+        @driver.navigate.to 'http://app.strava.com/activities/new'
+        puts '  Navigated to manual upload page'
+
+        # switch to run
+        element = @driver.find_element(:id, 'activity_type')
+        options = element.find_elements(:tag_name, 'option')
+        options.each do |option|
+            if option.attribute("value") == "Run"
+                option.click
+            end
+        end
+
+        upload_activity(run, 'Run')
     end
 
     def upload_ride(ride)
-        puts 'uploading ride: '
+        puts 'Uploading ride:'
 
         # navigate to manual activity upload page
-        puts '  navigating to manual upload page'
-        driver.navigate.to 'http://app.strava.com/activities/new'
-        puts '  navigated to manual upload page'
+        puts '  Navigating to manual upload page'
+        @driver.navigate.to 'http://app.strava.com/activities/new'
+        puts '  Navigated to manual upload page'
+
+        # switch to run
+        element = @driver.find_element(:id, 'activity_type')
+        options = element.find_elements(:tag_name, 'option')
+        options.each do |option|
+            if option.attribute("value") == "Ride"
+                option.click
+            end
+        end
+
+        upload_activity(ride, 'Ride')
     end
 
+private
+
+    def upload_activity(activity, type)
+
+        puts "    Setting activity distance:   #{activity.distance}"
+        element = @driver.find_element(:id, 'activity_distance')
+        element.clear
+        element.send_keys activity.distance
+
+        puts "    Setting activity duration:   #{activity.duration}"
+        element = @driver.find_element(:id, 'activity_elapsed_time')
+        element.clear
+        element.send_keys activity.duration
+
+        puts "    Setting activity start time: 09:00:00"
+        element = @driver.find_element(:id, 'activity_start_time_of_day')
+        element.clear
+        element.send_keys '09:00:00'
+        
+        puts "    Setting activity start date: #{activity.date}"
+        element = @driver.find_element(:id, 'activity_start_date')
+        element.clear
+        element.send_keys activity.date
+
+        puts "    Setting activity location:   Jersey"
+        element = @driver.find_element(:id, 'activity_input_location')
+        element.click
+        element.clear
+        element.send_keys 'Jersey'
+
+        puts "    Setting activity name:       #{activity.name}"
+        element = @driver.find_element(:id, 'activity_name')
+        element.click
+        element.clear
+        element.send_keys activity.name
+
+        # click submit button
+        element = @driver.find_element(:xpath, '//button')
+        puts '    Creating activity'
+        element.click
+        puts '    Created activity'
+
+        puts
+    end
 end
--- uploader/upload.rb
+++ uploader/upload.rb
@@ -2,24 +2,57 @@
 
 require './ride.rb'
 require './run.rb'
+require './strava_wrapper.rb'
+
+email = ARGV[0]
+password = ARGV[1]
+
+if email.nil? || password.nil?
+    puts "Please provide email and password arguments"
+    exit
+end
+
+puts "Strava credentials:"
+puts "  Email:    #{email}"
+puts "  Password: #{password}"
+puts
+
+strava = StravaWrapper.new
+strava.connect
+strava.login(email, password)
+
+# load runs file
+File.open('../runs.csv', 'r') do |file|
+    while (line = file.gets)
+        if line.length > 0
+            run = Run.new(line)
+            puts "RUN:  "
+            puts "  Name:     #{run.name}"
+            puts "  Date:     #{run.date}"
+            puts "  Duration: #{run.duration}"
+            puts "  Distance: #{run.distance}"
+            puts
+            strava.upload_run(run)
+        end
+    end
+    file.close
+end
 
 # load rides file
 File.open('../rides.csv', 'r') do |file|
     while (line = file.gets)
         if line.length > 0
             ride = Ride.new(line)
+            puts "RIDE: "
+            puts "  Name:     #{ride.name}"
+            puts "  Date:     #{ride.date}"
+            puts "  Duration: #{ride.duration}"
+            puts "  Distance: #{ride.distance}"
+            puts
+            strava.upload_ride(ride)
         end
     end
     file.close
 end
 
-# load runs file
-File.open('../runs.csv', 'r') do |file|
-    while (line = file.gets)
-        if line.length > 0
-            ride = Run.new(line)
-        end
-    end
-    file.close
-end
-
+strava.close
